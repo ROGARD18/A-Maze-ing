882152c0294460be390f3383a9001e81c67a89bc
Kheldin <kheldin.acherchour@gmail.com>
2026-03-05 10:01:17 +0100

    deleted range(3) in make first line

--- search/draw_maze.py
+++ search/draw_maze.py
@@ -1,28 +1,26 @@
 def make_first_maze_line(line: str) -> list[str]:
     lines_first_cell: list[str] = []
-    for i in range(3):
-        first_line = True if i == 0 else False
-        line_char = "▄" if first_line else " "
-        col_char = "▄" if first_line else "█"
+    line_char = " "
+    col_char = "█"
 
-        line_res = f"{col_char}"
-        for cell in line:
-            if cell in ('1', '2', '3', '8', '9', 'A', 'B'):
-                line_res += '    '
-                line_res += col_char if cell in ('9', 'A', 'B') else ' '
+    line_res = col_char
+    for cell in line:
+        if cell in ('1', '2', '3', '8', '9', 'A', 'B'):
+            line_res += '    '
+            line_res += col_char if cell in ('9', 'A', 'B') else ' '
+        else:
+            for _ in range(4):
+                line_res += line_char
+            if cell in ('C', 'D', 'E', 'F'):
+                line_res += col_char
             else:
-                for _ in range(4):
-                    line_res += line_char
-                if cell in ('C', 'D', 'E', 'F'):
-                    line_res += col_char
-                else:
-                    line_res += line_char
-        lines_first_cell.append(line_res)
+                line_res += line_char
+    lines_first_cell.append(line_res)
 
     return lines_first_cell
 
 
-def make_line(line: str, previous_line: str) -> str:
+def make_line(line: str, previous_line: str | None) -> str:
     line_res: str = "█"
     index: int = 0
 
@@ -32,7 +30,7 @@ def make_line(line: str, previous_line: str) -> str:
         else:
             line_res += '    '
         if cell in ('2', '3', '6', '7', 'A', 'B', 'E', 'F'):
-            if previous_line is not None and previous_line[index] in ('4', '5', '6', '7', 'C', 'D', 'E', 'F'):
+            if previous_line and previous_line[index] in ('4', '5', '6', '7', 'C', 'D', 'E', 'F'):
                 line_res += '█'
             else:
                 line_res += '▄'
